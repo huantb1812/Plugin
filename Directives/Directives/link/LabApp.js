63467de5057099b82f48c8a8419f934b789da783
huantb1812 <huantb1812@gmail.com>
2016-06-18 08:49:19 +0700

    tesst

--- Directives/Directives/link/LabApp.js
+++ Directives/Directives/link/LabApp.js
@@ -1,18 +1,1 @@
-﻿var modules = angular.module('LabApp', []);
-modules.controller('LabCtrl', function ($scope, $http) {
-});
-
-modules.directive('daoChieuNoiDung', function () {
-    return {
-        restrict: 'A',
-        link: function (scope, elem, attrs) {
-            var words = elem.text().split(' ');
-            var l = words.length;
-            var str = "";
-            for (var i = l - 1; i > 0; i--) {
-                str += " " + words[i];
-            }
-            elem.text(str);
-        }
-    }
-});
+﻿
